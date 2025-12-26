c403d8c102fb65be34a340a7baa0aa4de3b436e9
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-12-26 23:41:18 +0700

    Started encryption steps

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import secrets
 import csv
 from Crypto.Protocol.KDF import scrypt
 from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
 
 FILE_NAME = 'authenticate.csv'
 
@@ -26,13 +27,14 @@ def create_table():
     3. AEAD Algorithm
     4. Nonce
     5. Ciphertext
+    6. Tag (From the AES Crypto Library)
     """
 
     # https://www.geeksforgeeks.org/python/working-csv-files-python/
     if os.path.exists(FILE_NAME):
         return
 
-    fields = ['kdf', 'salt', 'aead', 'nonce', 'ciphertext']
+    fields = ['kdf', 'salt', 'aead', 'nonce', 'ciphertext', 'tag']
     with open(FILE_NAME, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
@@ -45,9 +47,37 @@ def main_menu():
     return choice
 
 
-def encrypt():
-    """Encrypt the file"""
+def encode_data(data):
+    """Convert file content to Bytes"""
     pass
+
+
+def encrypt():
+    """
+    Encrypt the File:
+
+    1. User select a file by pasting the filepath
+    2. User enter a password
+    3. A **salt** is created
+    4. A key created from salt and password (Using scrypt KDF)
+    5. A **nonce** and **tag** is created using the key (Using AES)
+        * The nonce is created automatically using Cryptodome AES
+    6. Encrypt the _content_ of the file with the Cipher
+    7. Store each content in the CSV
+    """
+    file_to_read = input("File Path: ")
+    if not os.path.exists(file_to_read):
+        print("File doesn't exist. Exiting...")
+        return
+
+    # Do everything else
+    password_str = (input("Input Password here: "))
+    password = password_str.encode()
+    salt = get_random_bytes(16)
+    key = scrypt(password, salt, 16, N=2**4, r=8, p=1)
+    cipher = AES.new(key, AES.MODE_GCM)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest()
 
 
 def decrypt():
