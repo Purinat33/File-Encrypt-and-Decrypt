d199fb5b23c6058d5f00e9481b2d5a16fba7b0b3
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-12-27 00:20:08 +0700

    Now we store content as base64 instead of bytes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import secrets
 import csv
+import base64
 from Crypto.Protocol.KDF import scrypt
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
@@ -13,6 +14,14 @@ File Encryptor/Decryptor by Purinat33
 --------------- <> ---------------
 """
 
+# For storage
+KDF = "scrypt"
+N = 2**4
+r = 8
+p = 1
+
+AEAD = 'AES-GCM'
+
 
 def create_table():
     """
@@ -22,19 +31,23 @@ def create_table():
 
     Structure:
 
-    1. KDF Algorithm|parameters
-    2. Salt
-    3. AEAD Algorithm
-    4. Nonce
-    5. Ciphertext
-    6. Tag (From the AES Crypto Library)
+    1. KDF Algorithm
+    2. KDF-N,
+    3. KDF-R,
+    4. KDF-P
+    5. Salt
+    6. AEAD Algorithm
+    7. Nonce
+    8. Ciphertext
+    9. Tag (From the AES Crypto Library)
     """
 
     # https://www.geeksforgeeks.org/python/working-csv-files-python/
     if os.path.exists(FILE_NAME):
         return
 
-    fields = ['kdf', 'salt', 'aead', 'nonce', 'ciphertext', 'tag']
+    fields = ['kdf', 'n', 'r', 'p', 'salt',
+              'aead', 'nonce', 'ciphertext', 'tag']
     with open(FILE_NAME, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
@@ -72,16 +85,24 @@ def encrypt():
     password_str = (input("Input Password here: "))
     password = password_str.encode()
     salt = get_random_bytes(16)
-    key = scrypt(password, salt, 16, N=2**4, r=8, p=1)
+    key = scrypt(password, salt, 16, N=N, r=r, p=p)
     cipher = AES.new(key, AES.MODE_GCM)
     nonce = cipher.nonce
 
     ciphertext, tag = cipher.encrypt_and_digest(file)
     # Storage
+    # Convert byte fields into base64
+    salt_b64 = base64.b64encode(salt)
+    nonce_b64 = base64.b64encode(nonce)
+    ciphertext_b64 = base64.b64encode(ciphertext)
+    tag_b64 = base64.b64encode(tag)
+
     with open(FILE_NAME, 'a') as f:
         csv_ = csv.writer(f)
         csv_.writerow(
-            f"scrypt,{salt.decode()},AES-GCM,{nonce.decode()},{ciphertext.decode()},{tag.decode()}")
+            [KDF, N, r, p, salt_b64.decode(), AEAD, nonce_b64.decode(),
+             ciphertext_b64.decode(), tag_b64.decode()]
+        )
 
 
 def decrypt():
